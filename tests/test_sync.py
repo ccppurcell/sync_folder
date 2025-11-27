c8a9251cd547075738809ef10032041b9b110475
ccppurcell <ccppurcell@gmail.com>
2025-11-27 15:14:49 +0100

    adds test_delete_file

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -65,3 +65,36 @@ def test_update_file(tmp_path):
     replica_file = test_replica / "test.txt"
     assert replica_file.read_text() == "modified"
 
+def test_delete_file(tmp_path):
+
+    #init the test source
+    test_source = tmp_path / "source"
+    test_source.mkdir()
+
+    #add a file to the test source
+    test_file = test_source / "test.txt"
+    test_file.write_text("test")
+
+    #init the replica
+    test_replica = tmp_path / "replica"
+    test_replica.mkdir()
+
+    #init the log file
+    test_log = tmp_path / "sync.log"
+    test_log.touch()
+
+    #init the app
+    app = Synchroniser(test_source, test_replica, 0, 1, test_log)
+
+    #run sync once to get identical contents in the source and replica
+    app.run()
+
+    #remove the test file
+    test_file.unlink()
+
+    #run the sync again
+    app.run()
+
+    #check the test file no longer exists in replica
+    replica_file = test_replica / "test.txt"
+    assert not replica_file.exists()
